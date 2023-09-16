220836a68105eda37f24a7c207e6b8a4a4f2116e
Augusto Renostro Remus <131462760+AugustoRemus@users.noreply.github.com>
2023-09-16 14:51:45 -0300

    Add files via upload

---
+++ trabalhoprog1/src/Main.java
@@ -0,0 +1,10 @@
+import java.text.CollationElementIterator;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+
+
